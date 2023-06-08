52cad8b89100d85de062132d6b86b7e4aa08b56c
Philipp Muens <philipp@muens.io>
2023-06-08 15:37:02 +0200

    Migrate code from nucypher/nufhe
    The migrated code can be found in commit hash 17f3b62.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='tfhe-py',
+    version='0.1.0',
+    description='Python implementation of the Fully Homomorphic Encryption scheme "TFHE"',
+    url='http://github.com/pmuens/tfhe-py',
+    author='Philipp Muens, Bogdan Opanchuk',
+    author_email='philipp@muens.io, bogdan@nucypher.com',
+    license='MIT',
+    packages=['tfhe'],
+    install_requires=['numpy>=1.24.3'],
+    zip_safe=True
+)
