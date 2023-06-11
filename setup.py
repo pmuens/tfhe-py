45a0205e98b5cede6ee2f98b728612dca37a584b
Philipp Muens <philipp@muens.io>
2023-06-11 11:47:12 +0200

    Update project description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="tfhe-py",
     version="0.1.0",
-    description='Python implementation of the Fully Homomorphic Encryption scheme "TFHE"',
+    description='A Python of "Fully Homomorphic Encryption over the Torus"',
     url="http://github.com/pmuens/tfhe-py",
     author="Philipp Muens, Bogdan Opanchuk",
     author_email="philipp@muens.io, bogdan@nucypher.com",
